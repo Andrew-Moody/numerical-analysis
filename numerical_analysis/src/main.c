40d8eb6ceb741c73b002fc1aab260664ed9d4740
Andrew-Moody <78827021+Andrew-Moody@users.noreply.github.com>
2024-01-24 19:08:05 -0600

    Add openmp parallel for test

--- numerical_analysis/src/main.c
+++ numerical_analysis/src/main.c
@@ -1,9 +1,55 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
 
 int main(void)
 {
-	printf("Hello from thread: %d\n", omp_get_thread_num());
+    int chunk = 1;
+    int size = 500000;
+    float* array = (float*)malloc(size * sizeof(float));
 
-	return 0;
+    if(!array)
+    {
+        printf("Failed to allocate array");
+        exit(1);
+    }
+
+    double startTime = omp_get_wtime();
+
+    #pragma omp parallel shared(array) num_threads(8)
+    {
+        #pragma omp single 
+        printf("Number of threads: %d\n", omp_get_num_threads());
+
+        printf("Hello from thread: %d\n", omp_get_thread_num());
+
+        #pragma omp for schedule(static, chunk)
+        for (int i = 0; i < size; ++i)
+        {
+            int n = 10001;
+            float total = 0;
+            for (int j = 0; j < n; ++j)
+            {
+                total += (float)j;
+            }
+            array[i] = (float)i * (2.0f * total / (float)n / (float)(n-1));
+        }
+
+        
+    }
+
+    double endTime = omp_get_wtime();
+
+    printf("%f\n", array[size - 1]);
+
+    /* for(int i = 0; i < size; ++i)
+    {
+        printf("%f\n", array[i]);
+    } */
+
+    printf("Time: %f\n", endTime - startTime);
+
+    free(array);
+
+    return 0;
 }
