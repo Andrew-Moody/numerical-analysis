3ff4e134c647fd14633e8fcb54dfee680ab3949d
Andrew-Moody <78827021+Andrew-Moody@users.noreply.github.com>
2024-02-29 23:48:40 -0600

    Add stiffness matrix coordinate transformation - Calculates a 3x3 transformation matrix that transforms forces and   displacements from the global reference frame to the local reference   frame of the element (x axis along element length, zero rotation   about x axis) - Expands the 3x3 matrix to 6x6 and uses it along with it's inverse   to transform the local stiffness matrix to the global frame before   adding its contribution to the global stiffness matrix

--- numerical_analysis/src/core/vector.h
+++ numerical_analysis/src/core/vector.h
@@ -71,3 +71,116 @@ static inline struct vec3 vec3_cross(struct vec3 vec1, struct vec3 vec2)
 
     return cross;
 }
+
+
+struct mat3
+{
+    float e11, e12, e13;
+    float e21, e22, e23;
+    float e31, e32, e33;
+
+    float elements[36];
+};
+
+static inline struct mat3 mat3_transpose(struct mat3 mat)
+{
+    struct mat3 transpose = {
+        mat.e11, mat.e21, mat.e31,
+        mat.e12, mat.e22, mat.e32,
+        mat.e13, mat.e23, mat.e33
+    };
+
+    return transpose;
+}
+
+/* static inline struct mat3 mat3_multiply(struct mat3 left, struct mat3 right)
+{
+    struct mat3 result = {
+        left.e11 * right.e11 + left.e12 * right.e21 + left.e13 * right.e31,
+        left.e11 * right.e12 + left.e12 * right.e22 + left.e13 * right.e32,
+        left.e11 * right.e13 + left.e12 * right.e23 + left.e13 * right.e33,
+
+        left.e21 * right.e11 + left.e22 * right.e21 + left.e23 * right.e31,
+        left.e21 * right.e12 + left.e22 * right.e22 + left.e23 * right.e32,
+        left.e21 * right.e13 + left.e22 * right.e23 + left.e23 * right.e33,
+
+        left.e31 * right.e11 + left.e32 * right.e21 + left.e33 * right.e31,
+        left.e31 * right.e12 + left.e32 * right.e22 + left.e33 * right.e32,
+        left.e31 * right.e13 + left.e32 * right.e23 + left.e33 * right.e33,
+    };
+
+    return result;
+} */
+
+
+static inline void mat3_multiply(struct mat3* result, struct mat3 left, struct mat3 right)
+{
+    for (int j = 0; j < 3; ++j)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            float v = 0;
+
+            for (int k = 0; k < 3; ++k)
+            {
+                v += left.elements[k + j * 3] * right.elements[i + 3 * k];
+            }
+
+            result->elements[i + j * 3] = v;
+        }
+    }
+}
+
+static inline struct vec3 mat3_premultiply(struct mat3 mat, struct vec3 vec)
+{
+    struct vec3 result = {
+        mat.e11 * vec.x + mat.e12 * vec.y + mat.e13 * vec.z,
+        mat.e21 * vec.x + mat.e22 * vec.y + mat.e23 * vec.z,
+        mat.e31 * vec.x + mat.e32 * vec.y + mat.e33 * vec.z,
+    };
+
+    return result;
+}
+
+static inline struct vec3 mat3_postmultiply(struct vec3 vec, struct mat3 mat)
+{
+    struct vec3 result = {
+        mat.e11 * vec.x + mat.e21 * vec.y + mat.e31 * vec.z,
+        mat.e12 * vec.x + mat.e22 * vec.y + mat.e32 * vec.z,
+        mat.e13 * vec.x + mat.e23 * vec.y + mat.e33 * vec.z,
+    };
+
+    return result;
+}
+
+
+static inline void mat6_multiply(float* result, float* left, float* right)
+{
+    for (int j = 0; j < 6; ++j)
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            float v = 0;
+
+            for (int k = 0; k < 6; ++k)
+            {
+                v += left[k + j * 6] * right[i + 6 * k];
+            }
+
+            result[i + j * 6] = v;
+        }
+    }
+}
+
+static inline void mat6_transpose(float* mat)
+{
+    for (int j = 0; j < 6; ++j)
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            float temp = mat[i + j * 6];
+            mat[i + j * 6] = mat[j + i * 6];
+            mat[j + i * 6] = temp;
+        }
+    }
+}
--- numerical_analysis/src/structural/frame.c
+++ numerical_analysis/src/structural/frame.c
@@ -27,8 +27,58 @@ struct Element
 };
 
 
-void add_local_stiffness(float* k_global, float* k_local, int node1, int node2, int node_count)
+void add_local_stiffness(float* k_global, float* k_local, struct mat3 transform, int node1, int node2, int node_count)
 {
+    // Inverse transformation matrix is its transpose
+    struct mat3 transinv = mat3_transpose(transform);
+
+    float transform6x6[36];
+
+    for (int j = 0; j < 6; ++j)
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            transform6x6[i + j * 6] = 0;
+        }
+    }
+
+    transform6x6[0] = transform.e11;
+    transform6x6[1] = transform.e12;
+    transform6x6[2] = transform.e13;
+
+    transform6x6[6] = transform.e21;
+    transform6x6[7] = transform.e22;
+    transform6x6[8] = transform.e23;
+
+    transform6x6[12] = transform.e31;
+    transform6x6[13] = transform.e32;
+    transform6x6[14] = transform.e33;
+
+    transform6x6[21] = transform.e11;
+    transform6x6[22] = transform.e12;
+    transform6x6[23] = transform.e13;
+
+    transform6x6[27] = transform.e21;
+    transform6x6[28] = transform.e22;
+    transform6x6[29] = transform.e23;
+
+    transform6x6[33] = transform.e31;
+    transform6x6[34] = transform.e32;
+    transform6x6[35] = transform.e33;
+
+    float invtrans6x6[36];
+    for (int i = 0; i < 36; ++i)
+    {
+        invtrans6x6[i] = transform6x6[i];
+    }
+
+    mat6_transpose(invtrans6x6);
+
+    float kxt[36];
+    mat6_multiply(kxt, k_local, transform6x6);
+    float k_g[36];
+    mat6_multiply(k_g, invtrans6x6, kxt);
+
     int offset = 6 * node1 + 36 * (node2 * node_count);
 
     for (int j = 0; j < 6; ++j)
@@ -39,7 +89,7 @@ void add_local_stiffness(float* k_global, float* k_local, int node1, int node2, 
 
             int global_idx = i + offset + j * 6 * node_count;
 
-            k_global[global_idx] += k_local[local_idx];
+            k_global[global_idx] += k_g[local_idx];
         }
     }
 }
@@ -80,12 +130,12 @@ void structural(void)
 
     // Define node positions
     nodes[0].pos = (struct vec3){ 0.f, 0.f, 0.f };
-    nodes[1].pos = (struct vec3){ 1.f, 0.f, 0.f };
+    nodes[1].pos = (struct vec3){ 0.f, 1.f, 0.f };
 
     // Define boundary conditions (node1 is fixed, node2 has a force applied)
     nodes[0].displacement = (struct vec3){ 0.f, 0.f, 0.f };
     nodes[0].rotation = (struct vec3){ 0.f, 0.f, 0.f };
-    nodes[1].force = (struct vec3){ 1.f, 0.f, 0.f };
+    nodes[1].force = (struct vec3){ 1.f, 1.f, 1.f };
 
     // Define an element between node1 and node2
     elements[0] = (struct Element){ 0, 1, elastic_modulus, shear_modulus, 1.f };
@@ -153,13 +203,99 @@ void structural(void)
             0, 0, 0, 0, 0, 0,
         };
 
-        // Will need to transform local to global space here
+        // The stiffness matrices above assume the element is lying in the global reference frame
+        // with the length oriented along the global x axis
+        // Of course the elements can be in any orientation so the global displacements do not relate
+        // to global forces via these stiffness matrices but instead relate in the local frame
+        // ie. F_l = K * U_l
+
+        // If we have a matrix T that represents the transformation from global to local frame
+        // we can transform the forces and displacements F_l = T * F_g, U_l = T * U_g
+        // relating via local stiffness K gives T * F_g = K * T * U_g
+        // Pre-multiplying both sides by T inverse gives F_g = T^-1 * K * T * U_g
+        // From here it can be seen that the stiffness matrix that relates global displacements
+        // to global forces is K_g = T^-1 * K * T
+        // so before contributing the elements stiffness to the global stiffness matrix
+        // we must first find the global to local transformation matrix T for the element
+
+        // first subtract global position of node1 from global position of node2 to give
+        // a global vector that lies along the length of the element (so along local x axis)
+        // Most likely an element in the frame is not rotated about local x when in an unstressed position
+        // so it would be fine to assume local theta_x is 0 but a boundary condition could impose a rotation about x
+
+        // If we do make the assumption that theta_x is zero then we know that a global vector pointing in the global y direction
+        // is incident on the local x-y plane of the element and likewise a global vector in the z direction is incident
+        // on the local x-z plane so we can use either as a starting point we just need one where the magnitude of the crossproduct
+        // local x axis is not close enough to zero to cause floating point errors when normalizing
+        // this can be checked easily by checking if the dot product magnitude is close to 1 and picking the other vector if so
+
+        // normalize the element axial vector to give the local x axis in global frame
+        struct vec3 x_axis = vec3_subtract(nodes[node2].pos, nodes[node1].pos);
+        x_axis = vec3_normalize(x_axis);
+
+        // Default initialize y and x axis
+        struct vec3 y_axis = { 0.f, 0.f, 0.f };
+        struct vec3 z_axis = { 0.f, 0.f, 0.f };
+
+        // We need a second vector that is not to close to parallel with local x
+        // to produce a cross product with reasonable magnitude
+        // if the dot product magnitude between local x and global y is small
+        // then they are close to alignment (regardless of direction) and global z
+        // becomes a better choice
+        float dot = abs(vec3_dot(x_axis, (struct vec3) { 0.f, 1.f, 0.f }));
+        printf("dot: %f\n", dot);
+        if (dot < 0.5f)
+        {
+            // local y is reasonably close to global y so the local z axis in global frame
+            // is the normalized cross product between the local x axis in global frame and the global y axis
+            z_axis = vec3_cross(x_axis, (struct vec3) { 0.f, 1.f, 0.f });
+            z_axis = vec3_normalize(z_axis);
+
+            // Now local y is just cross product of local z and local x
+            y_axis = vec3_cross(z_axis, x_axis);
+            y_axis = vec3_normalize(y_axis); // should be unit length already but might as well make sure
+
+            printf("x_axis: %2.f, %2.f, %2.f\n", x_axis.x, x_axis.y, x_axis.z);
+            printf("y_axis: %2.f, %2.f, %2.f\n", y_axis.x, y_axis.y, y_axis.z);
+            printf("z_axis: %2.f, %2.f, %2.f\n", z_axis.x, z_axis.y, z_axis.z);
+        }
+        else
+        {
+            // local z is reasonably close to global z so the local y axis in global frame
+            // is the normalized cross product between the global z axis and the local x axis in global frame
+            // (could have also done vec3_cross(x_axis, (struct vec3) { 0.f, 0.f, -1.f }) just comfirm with right hand rule)
+            y_axis = vec3_cross((struct vec3) { 0.f, 0.f, 1.f }, x_axis);
+
+            printf("z_axis: %2.1f, %2.1f, %2.1f\nn", z_axis.x, z_axis.y, z_axis.z);
+
+            y_axis = vec3_normalize(y_axis);
+
+            // Now local z is just cross product of local x and local y
+            z_axis = vec3_cross(x_axis, y_axis);
+            z_axis = vec3_normalize(z_axis); // should be unit length already but might as well make sure
+
+            printf("x_axis: %2.f, %2.f, %2.f\n", x_axis.x, x_axis.y, x_axis.z);
+            printf("y_axis: %2.f, %2.f, %2.f\n", y_axis.x, y_axis.y, y_axis.z);
+            printf("z_axis: %2.f, %2.f, %2.f\n", z_axis.x, z_axis.y, z_axis.z);
+        }
+
+        // Build the transformation matrix from global to local for this element
+        struct mat3 transform = {
+            x_axis.x, x_axis.y, x_axis.z,
+            y_axis.x, y_axis.y, y_axis.z,
+            z_axis.x, z_axis.y, z_axis.z,
+        };
+
+
+        /* printf("x_axis: %2.f, %2.f, %2.f\n", x_axis.x, x_axis.y, x_axis.z);
+        printf("y_axis: %2.f, %2.f, %2.f\n", y_axis.x, y_axis.y, y_axis.z);
+        printf("z_axis: %2.f, %2.f, %2.f\n", z_axis.x, z_axis.y, z_axis.z); */
 
         // Add the local contributions to the global stiffness matrix
-        add_local_stiffness(k_global, k11, node1, node1, node_count);
-        add_local_stiffness(k_global, k12, node1, node2, node_count);
-        add_local_stiffness(k_global, k21, node2, node1, node_count);
-        add_local_stiffness(k_global, k22, node2, node2, node_count);
+        add_local_stiffness(k_global, k11, transform, node1, node1, node_count);
+        add_local_stiffness(k_global, k12, transform, node1, node2, node_count);
+        add_local_stiffness(k_global, k21, transform, node2, node1, node_count);
+        add_local_stiffness(k_global, k22, transform, node2, node2, node_count);
     }
 
 
